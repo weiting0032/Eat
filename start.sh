6d02ce44b292da457b0d4211a7e188fabbcd326a
weiting0032 <42844595+weiting0032@users.noreply.github.com>
2026-04-07 09:59:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,20 @@ set -e
 mkdir -p data
 mkdir -p uploads
 
+echo "Python version:"
 python --version
+
+echo "Pip version:"
 pip --version
+
+echo "Installing setuptools/wheel..."
+python -m pip install --upgrade pip
+python -m pip install setuptools wheel
+
+echo "Testing pkg_resources..."
+python - <<'PY'
+import pkg_resources
+print("pkg_resources OK")
+PY
 
 exec supervisord -c supervisord.conf
